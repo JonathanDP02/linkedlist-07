8922086e34128ebf657b67a57356554e0d2d7a10
JonathanDP02 <jonatanpangestu23@gmail.com>
2025-06-01 18:12:20 +0700

    w

--- SLLmain07.java
+++ SLLmain07.java
@@ -2,10 +2,10 @@ public class SLLmain07 {
     public static void main(String[] args) {
         singleLinkedKist07 sll = new singleLinkedKist07();
         
-        mahasiswa07 mhs1 = new mahasiswa07("123", "Alice", "A", 3.5);
-        mahasiswa07 mhs2 = new mahasiswa07("456", "Bob", "B", 3.8);
-        mahasiswa07 mhs3 = new mahasiswa07("789", "Charlie", "C", 3.2);
-        mahasiswa07 mhs4 = new mahasiswa07("101", "Diana", "D", 3.9);
+        mahasiswa07 mhs1 = new mahasiswa07("123", "Jon", "1A", 3.5);
+        mahasiswa07 mhs2 = new mahasiswa07("456", "Nathan", "2B", 3.8);
+        mahasiswa07 mhs3 = new mahasiswa07("789", "Dwi", "2C", 3.2);
+        mahasiswa07 mhs4 = new mahasiswa07("101", "Pangestu", "1D", 3.9);
 
         sll.print();
         sll.addFirst(mhs4);
